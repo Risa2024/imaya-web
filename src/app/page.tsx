54935657f1ae750cceb004db70ccdf14806886a8
Risa Goto <omusubi5656dup@gmail.com>
2025-01-13 19:52:54 +0900

    fix:写真追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,49 +11,56 @@ import {
 
 export default function WelcomePage() {
   return (
-    <Carousel
-      className='z-0'
-      plugins={[
-        Autoplay({
-          delay: 5000,
-        }),
-      ]}
-      opts={{
-        align: 'start',
-        loop: true,
-      }}
-    >
-      <CarouselContent>
-        <CarouselItem>
-          <Image
-            src='/kitchen.jpg'
-            alt='Carousel Image 1'
-            width={1200}
-            height={800}
-            className='h-auto w-full object-cover'
-          />
-        </CarouselItem>
-        <CarouselItem>
-          <Image
-            src='/tomato.jpg'
-            alt='Carousel Image 2'
-            width={1200}
-            height={800}
-            className='h-auto w-full object-cover'
-          />
-        </CarouselItem>
-        <CarouselItem>
-          <Image
-            src='/sosegi.jpg'
-            alt='Carouel Image 1'
-            width={1200}
-            height={800}
-            className='h-auto w-full object-cover'
-          />
-        </CarouselItem>
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <>
+      <Carousel
+        className='z-0'
+        plugins={[
+          Autoplay({
+            delay: 5000,
+          }),
+        ]}
+        opts={{
+          align: 'start',
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          <CarouselItem>
+            <Image
+              src='/kitchen.jpg'
+              alt='Carousel Image 1'
+              width={1200}
+              height={800}
+              className='h-auto w-full object-cover'
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src='/tomato.jpg'
+              alt='Carousel Image 2'
+              width={1200}
+              height={800}
+              className='h-auto w-full object-cover'
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src='/sosegi.jpg'
+              alt='Carouel Image 1'
+              width={1200}
+              height={800}
+              className='h-auto w-full object-cover'
+            />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      {/* New Content Section */}
+      <section className='container flex flex-col items-center justify-between p-2'>
+        <p>今屋のハンバーガーとは</p>
+      </section>
+    </>
   );
 }
