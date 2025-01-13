460e2460633e707b40cf7fad47152d2281ea9ce6
Risa Goto <omusubi5656dup@gmail.com>
2025-01-13 18:30:20 +0900

    add:カルーセル機能

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+import Autoplay from 'embla-carousel-autoplay';
+import Image from 'next/image';
 import {
   Carousel,
   CarouselContent,
@@ -6,13 +8,48 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+
 export default function WelcomePage() {
   return (
-    <Carousel>
+    <Carousel
+      plugins={[
+        Autoplay({
+          delay: 5000,
+        }),
+      ]}
+      opts={{
+        align: 'start',
+        loop: true,
+      }}
+    >
       <CarouselContent>
-        <CarouselItem>...</CarouselItem>
-        <CarouselItem>...</CarouselItem>
-        <CarouselItem>...</CarouselItem>
+        <CarouselItem>
+          <Image
+            src='/kitchen.jpg'
+            alt='Carousel Image 1'
+            width={1200}
+            height={800}
+            className='h-auto w-full object-cover'
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <Image
+            src='/tomato.jpg'
+            alt='Carousel Image 2'
+            width={1200}
+            height={800}
+            className='h-auto w-full object-cover'
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <Image
+            src='/sosegi.jpg'
+            alt='Carouel Image 1'
+            width={1200}
+            height={800}
+            className='h-auto w-full object-cover'
+          />
+        </CarouselItem>
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
