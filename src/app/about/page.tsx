7a3f389603cff2d3ee30d7ee2675c6916ccba1b3
akkie8 <akkie.819@gmail.com>
2025-01-13 18:48:54 +0900

    add: git env example

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,25 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
+import NewsList from '@/components/common/NewsList';
+import { client } from '@/lib/microcms';
+import { News } from '@/types/microcms';
 
-export default function CompanyInfo() {
+async function getNewsList() {
+  const response = await client.get({
+    endpoint: 'news', // エンドポイントを'news'に変更
+    queries: { limit: 10 },
+  });
+  return response.contents as News[];
+}
+
+export default async function CompanyInfo() {
+  const newsList = await getNewsList();
+
   return (
     <div className='container mx-0 max-w-md px-4 py-10 text-sm leading-6 tracking-widest md:mx-auto md:max-w-xl'>
+      <Suspense fallback={<div>Loading...</div>}>
+        <NewsList news={newsList} />
+      </Suspense>
       <h1 className='mb-8 text-base font-medium md:text-center'>運営者情報</h1>
       <h2 className='mb-2 text-sm'>サービス名</h2>
       <p className='mb-5 border-b pb-5 tracking-widest'>Moodify</p>
