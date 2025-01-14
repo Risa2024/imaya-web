e506d36f99d19cc38aa5fefa8583f6c4b5c04af9
akkie8 <akkie.819@gmail.com>
2025-01-14 10:33:51 +0900

    fix: フォントをNoto Sansに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,20 @@
 import type { Metadata } from 'next';
-import { Kosugi, Alegreya } from 'next/font/google';
+import { Noto_Sans } from 'next/font/google';
 import '@/styles/globals.css';
 import { MainHeader } from '@/components/dashboard/MainHeader';
 //import { MainHeaderTest } from '@/components/dashboard/MainHeaderTest';
 import { MainFooter } from '@/components/common/MainFooter';
-
 export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'imaya',
   description: 'imaya is a shop in Ehime specializing in handmade kitchen car .',
 };
 
-export const kosugi = Kosugi({
-  weight: '400',
+const notoSans = Noto_Sans({
   subsets: ['latin'],
+  weight: ['400', '700'],
   display: 'swap',
-  preload: true,
-  variable: '--font-kosugi',
-});
-
-export const alegreya = Alegreya({
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
-  variable: '--font-alegreya',
 });
 
 export default function RootLayout({
@@ -32,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ja' className={`h-full ${kosugi.variable} ${alegreya.variable}`}>
+    <html lang='ja' className={`h-full ${notoSans.className}`}>
       <body className='flex min-h-screen flex-col'>
         <MainHeader />
         {/* <MainHeaderTest /> */}
