92847b9e42ff8052618750a1c42863428acd15b1
akkie8 <akkie.819@gmail.com>
2025-01-14 11:12:58 +0900

    fix: ビルドエラーの修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
-import { Kosugi, Alegreya } from 'next/font/google';
+// import { Kosugi, Alegreya } from 'next/font/google';
 import '@/styles/globals.css';
-import { MainHeader } from '@/components/dashboard/MainHeader';
+import { MainHeader } from '@/components/common/MainHeader';
 //import { MainHeaderTest } from '@/components/dashboard/MainHeaderTest';
 import { MainFooter } from '@/components/common/MainFooter';
 
@@ -11,20 +11,20 @@ export const metadata: Metadata = {
   description: 'imaya is a shop in Ehime specializing in handmade kitchen car .',
 };
 
-export const kosugi = Kosugi({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
-  variable: '--font-kosugi',
-});
+// export const kosugi = Kosugi({
+//   weight: '400',
+//   subsets: ['latin'],
+//   display: 'swap',
+//   preload: true,
+//   variable: '--font-kosugi',
+// });
 
-export const alegreya = Alegreya({
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
-  variable: '--font-alegreya',
-});
+// export const alegreya = Alegreya({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   preload: true,
+//   variable: '--font-alegreya',
+// });
 
 export default function RootLayout({
   children,
@@ -32,7 +32,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ja' className={`h-full ${kosugi.variable} ${alegreya.variable}`}>
+    // <html lang='ja' className={`h-full ${kosugi.variable} ${alegreya.variable}`}>
+    <html lang='ja' className={`h-full`}>
       <body className='flex min-h-screen flex-col'>
         <MainHeader />
         {/* <MainHeaderTest /> */}
