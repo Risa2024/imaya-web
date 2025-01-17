fb264a558b3783723f27ba27e98193f97defa641
Risa Goto <omusubi5656dup@gmail.com>
2025-01-17 10:53:40 +0900

    add:歴史の写真、説明追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { CarouselSection } from '@/components/CarouselSection';
 import { NewsList } from '@/components/common/NewsList';
 import { client } from '@/lib/microcms';
 import { News } from '@/types/microcms';
+import Image from 'next/image';
 
 async function getNewsList() {
   try {
@@ -41,6 +42,17 @@ export default async function WelcomePage() {
             <NewsList news={newsList} />
           </div>
         </Suspense>
+        <div className='flex flex-col items-center md:flex-row'>
+          <Image src='/dish.jpg' alt='料理の画像' width={500} height={300} />
+          <h1 className='text-2xl font-bold'>今屋のハンバーガーの歴史</h1>
+          <p>
+            今屋のハンバーガーは、今から50年前、福岡市の西公園の展望台で「今崎勝美」さんによって生まれました。
+            <br />
+            ホットドックにかける熱い情熱と、平和を願う温かい人柄に惹かれ日本全国、また世界中から絶え間なくお客様が訪れます。
+            <br />
+            幻のハンバーガーを味わいたいと、３時間待ちは当たり前の大人気店です。
+          </p>
+        </div>
       </div>
     </>
   );
