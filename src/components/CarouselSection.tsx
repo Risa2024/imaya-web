d50960a7f16a41b465a82dc7b0ca395a4e9d4c05
Risa Goto <omusubi5656dup@gmail.com>
2025-01-24 15:46:01 +0900

    add:カルーセルレスポンシブ

--- src/components/CarouselSection.tsx
+++ src/components/CarouselSection.tsx
@@ -27,29 +27,50 @@ export function CarouselSection() {
       <CarouselContent className=''>
         <CarouselItem>
           <Image
+            src='/kitchenLg.jpg'
+            alt='Carousel Image 1'
+            width={1200}
+            height={630}
+            className='h-auto w-full object-cover md:hidden'
+          />
+          <Image
             src='/kitchen.png'
             alt='Carousel Image 1'
             width={1200}
             height={630}
-            className='h-auto w-full object-cover'
+            className='hidden h-auto w-full object-cover md:block'
           />
         </CarouselItem>
         <CarouselItem>
+          <Image
+            src='/tomatoLg.jpg'
+            alt='Carousel Image 2'
+            width={1200}
+            height={630}
+            className='h-auto w-full object-cover md:hidden'
+          />
           <Image
             src='/tomato.png'
             alt='Carousel Image 2'
             width={1200}
             height={630}
-            className='h-auto w-full object-cover'
+            className='hidden h-auto w-full object-cover md:block'
           />
         </CarouselItem>
         <CarouselItem>
+          <Image
+            src='/sosegiLg.jpg'
+            alt='Carousel Image 3'
+            width={1200}
+            height={630}
+            className='h-auto w-full object-cover md:hidden'
+          />
           <Image
             src='/sosegi.png'
             alt='Carousel Image 3'
             width={1200}
             height={630}
-            className='h-auto w-full object-cover'
+            className='hidden h-auto w-full object-cover md:block'
           />
         </CarouselItem>
       </CarouselContent>
