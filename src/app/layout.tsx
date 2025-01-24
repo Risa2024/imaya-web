e935abae10816407c587199706ee24f043a61238
Risa Goto <omusubi5656dup@gmail.com>
2025-01-24 10:40:55 +0900

    fix:フォント

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from 'next';
-// import { Kosugi, Alegreya } from 'next/font/google';
+import { Noto_Sans_JP } from 'next/font/google';
 import '@/styles/globals.css';
 import { MainHeader } from '@/components/common/MainHeader';
 //import { MainHeaderTest } from '@/components/dashboard/MainHeaderTest';
 //import { MainFooter } from '@/components/common/MainFooter';
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'], // 必要な太さを指定
+  display: 'swap',
+});
 
 export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
@@ -32,8 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <html lang='ja' className={`h-full ${kosugi.variable} ${alegreya.variable}`}>
-    <html lang='ja' className={`h-full`}>
+    <html lang='ja' className={`h-full ${notoSansJP.className}`}>
       <body className='flex min-h-screen flex-col'>
         <MainHeader />
         {/* <MainHeaderTest /> */}
