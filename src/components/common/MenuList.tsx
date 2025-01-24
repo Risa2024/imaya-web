a9ea72d68591b83ed6fdec47106e7e1d92d6829d
Risa Goto <omusubi5656dup@gmail.com>
2025-01-24 15:29:44 +0900

    fix:cols

--- src/components/common/MenuList.tsx
+++ src/components/common/MenuList.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 export const MenuList = () => {
   return (
-    <div className='mx-auto grid max-w-7xl grid-cols-2 gap-6 p-4 md:grid-cols-3'>
+    <div className='mx-auto grid max-w-7xl gap-6 p-4 md:grid-cols-3'>
       <div className='overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300'>
         <div className='relative h-48 w-full'>
           <Image src='/mixegg.jpg' alt='ミックスエッグ' fill className='object-contain' />
         </div>
         <div className='p-4 text-center'>
-          <h3 className='text-lg font-bold text-gray-800'>ミックスエッグ</h3>
+          <h3 className='text-lg font-bold text-gray-800 md:text-lg'>ミックスエッグ</h3>
           <p className='text-sm text-gray-500'>MIXED EGG</p>
           <p className='mt-2 text-sm font-bold text-gray-700'>500円</p>
         </div>
