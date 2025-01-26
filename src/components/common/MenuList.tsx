903fdff1ecd1c902a4bb80522649f33fbbf081c0
Risa Goto <omusubi5656dup@gmail.com>
2025-01-26 16:53:34 +0900

    fix:メニュー画像

--- src/components/common/MenuList.tsx
+++ src/components/common/MenuList.tsx
@@ -8,7 +8,7 @@ export const MenuList = () => {
       {/* メニューヘッダー */}
       <h2 className='mb-2 text-center text-sm font-medium text-muted-foreground'>menu</h2>
       <div id='menu' className='mb-20 flex items-center justify-center'>
-        <Image src='/menu.jpg' alt='メニューの画像' width={210} height={90} className='' />
+        <Image src='/menu.png' alt='メニューの画像' width={210} height={90} className='' />
       </div>
       {/* メニューグリッド */}
       <div className='mx-auto grid max-w-7xl grid-cols-2 gap-3 md:grid-cols-3 md:gap-6 lg:grid-cols-4'>
