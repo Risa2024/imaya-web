e3ab9b7cb06fdeed960bf07733976bfd044fb3ba
Risa Goto <omusubi5656dup@gmail.com>
2025-01-27 16:45:54 +0900

    fix:クリームチーズ誤字修正

--- src/components/common/MenuList.tsx
+++ src/components/common/MenuList.tsx
@@ -202,7 +202,7 @@ export function MenuList() {
             <h3 className='text-base font-bold text-gray-800 md:text-lg'>
               100%北海道クリームチーズ
             </h3>
-            <p className='text-xs text-gray-500 md:text-sm'>MIXED EGG</p>
+            <p className='text-xs text-gray-500 md:text-sm'>CREAM CHEESE</p>
             <p className='mt-1 text-sm font-medium text-orange-600 md:text-base'>600円</p>
           </div>
         </div>
